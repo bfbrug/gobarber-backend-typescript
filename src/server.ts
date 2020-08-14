5df63f202efff669e401b2c34d0289f0750c7007
Bruno <bfbrug@gmail.com>
2020-08-14 12:34:01 -0400

    install cors: refature

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -12,6 +13,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 

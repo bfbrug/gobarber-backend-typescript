1b4853f5bcc15d2d1123a74aba357b1fc19c8404
Bruno Fernandes <bfbrug@gmail.com>
2021-01-24 21:18:54 -0400

    Modificando estrutura para DDD - SOLID

---
+++ src/modules/appointments/services/CreateAppoinmentService.ts
@@ -0,0 +1,36 @@
+import { startOfHour } from 'date-fns';
+
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppoinmentService {
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+
+  public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentIsSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentIsSameDate) {
+      throw new AppError('This appoiment is already booked!');
+    }
+
+    const appointmet = await this.appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    return appointmet;
+  }
+}
+
+export default CreateAppoinmentService;

d281f33a4bed537a63aa299c0913774578cd0eb8
Bruno Fernandes <bfbrug@gmail.com>
2021-02-01 10:36:37 -0400

    Criada listagem de prestadores

---
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProvidersController from '../controllers/ProvidersController';
+
+const providersRouter = Router();
+const providersControler = new ProvidersController();
+
+providersRouter.use(ensureAuthenticated);
+
+providersRouter.get('/', providersControler.index);
+export default providersRouter;
